998e5aacdfa7f84f539c075bd006f48f7f880dfb
Rafi <delwar2021bd@gmail.com>
2024-01-15 18:41:58 +0600

    inital setup

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+
+const AboutPage = () => {
+
+    return (
+        <div>
+            <p className="text-center font-bold text-4xl">This is about page</p>
+        </div>
+    );
+};
+
+export default AboutPage;
