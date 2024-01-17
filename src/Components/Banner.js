059bc1e2b2ad5c4ee000edb0d58f980cb9d99524
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-01-17 12:39:47 +0600

    top header design completed

---
+++ src/Components/Banner.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div className='bg-[url("https://i.ibb.co/N1VHksh/Black-And-Red-Modern-Business-Human-Resource-Management-Presentation.png")] w-full min-h-screen absolute top-0 left-0 -z-10'>
+            {/* <Image className="w-full -z-10 absolute top-0 left-0" src="https://i.ibb.co/N1VHksh/Black-And-Red-Modern-Business-Human-Resource-Management-Presentation.png" height={2000} width={2000} /> */}
+            {/* <p className='py-24 pt-40 '>lorema asdj </p> */}
+        </div>
+    )
+}
+
+export default Banner
