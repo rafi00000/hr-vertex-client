11306ce0a6dd31d9655db9a9656ce02bbeaaf5dc
Mohammadhridoy <mdridoykha019@gmail.com>
2024-01-24 12:52:11 +0600

    create context api and set main layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import ContextProvider from '@/provider/ContextProvider'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,9 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <body className={inter.className}>
+        <ContextProvider>
         <div className='min-h-screen'>
         {children}
+
         </div>
+        </ContextProvider>
       </body>
     </html>
   )
