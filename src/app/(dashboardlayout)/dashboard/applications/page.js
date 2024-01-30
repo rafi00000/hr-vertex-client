ebb5bee8a7bc99f5fe90e5b503cea8e0d249d7ae
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-01-30 19:16:21 +0600

    create all application table

--- src/app/(dashboardlayout)/dashboard/applications/page.js
+++ src/app/(dashboardlayout)/dashboard/applications/page.js
@@ -3,7 +3,7 @@
 import useAxiosSecure from "@/axiosConfig/useAxiosSecure";
 import { useState } from "react";
 
-const usePage = () => {
+const usePage = async() => {
     const [show, setshow] = useState(false)
     const axiosSecure = useAxiosSecure()
     const HandelSubmit = async (e) => {
@@ -21,9 +21,40 @@ const usePage = () => {
         // const res = await axiosSecure.post('/recruitment', data)
         // console.log(res.data);
     }
+   try {
+    const res = await axiosSecure.get('/users', { params: { next: { revalidate: 100 } } });
+    const appicationdata = res.data
     return (
         <div className="p-5 container  mx-auto my-4  rounded-lg h-screen overflow-y-scroll">
             <h3 className="text-center pt-10 uppercase text-3xl font-bold">all applications  </h3>
+            <div className="overflow-x-auto" >
+                <h3 className='uppercase text-3xl font-bold pt-10 pb-3 text-center'>all employees</h3>
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th className='font-bold'>Candidate</th>
+                            <th className='font-bold'>email</th>
+                            <th className='font-bold'>job</th>
+                            <th className='font-bold'>action </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            appicationdata.map(item => <tr key={item?._id} className="bg-base-200">
+                                <td>{item?.name}</td>
+                                <td>{item?.email}</td>
+                                <td>{item?.job}</td>
+                                <td>
+                                    <Button text='delete' />
+                                </td>
+
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div >
             {
                 show && <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <form onSubmit={HandelSubmit} className="space-y-4 md:space-y-6" action="#">
@@ -56,6 +87,10 @@ const usePage = () => {
 
         </div>
     )
+   } catch (error) {
+    console.error("Error fetching user data:", error);
+    return <div className='text-center text-4xl font-bold py-20'>Error loading application data data</div>;
+   }
 }
 
 export default usePage
