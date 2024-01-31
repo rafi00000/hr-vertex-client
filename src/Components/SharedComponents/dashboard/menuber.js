60fb054fb8c73e414d9f1653d7c547d42c66deac
Rafi <delwar2021bd@gmail.com>
2024-01-31 10:32:13 +0600

    mod: dashboard design, add: profile page

--- src/Components/SharedComponents/dashboard/menuber.js
+++ src/Components/SharedComponents/dashboard/menuber.js
@@ -1,5 +1,5 @@
-'use client'
-import Image from "next/image"
+"use client";
+import Image from "next/image";
 import Link from "next/link";
 import { useContext, useState } from "react";
 import { RiUserSearchFill } from "react-icons/ri";
@@ -11,60 +11,75 @@ import { IoIosHome } from "react-icons/io";
 import { coreContext } from "@/provider/AuthContext";
 import { RiFileCopy2Fill } from "react-icons/ri";
 const Menuber = () => {
-    const {user}=useContext(coreContext)
-    const [show, setshow] = useState(false)
-    return (
-        <div className={`pl-5 bg-black md:min-h-screen bg-opacity-10 py-3 shadow-2xl relative ${show ? 'h-auto' : 'h-[65px]'} overflow-hidden`}>
-            <div className="flex justify-start items-center  gap-4 border-b border-black pb-3">
-                <Image className="w-10 h-10 object-cover rounded-full" src={user?.photoURL} height={500} width={500} alt="img" />
-                <span className="flex flex-col justify-start items-start gap-0">
-                    <p className="text-lg font-bold">{user?.displayName}</p>
-                    <a className="-mt-1 text-xs" href="mailto:useremail">{user?.email}</a>
-                </span>
-            </div>
-            <TiThMenuOutline onClick={() => setshow(!show)} className="absolute transition-all md:hidden block top-6 right-4 text-2xl font-bold" />
-            <ul className="pt-3 uppercase font-semibold flex flex-col gap-2 ">
-                <li className="flex justify-start items-center gap-2">
-                    <CgProfile className="text-xl" />
-                    <Link href={`#`}>
-                        profile
-                    </Link>
-                </li>
-                <li className="flex justify-start items-center gap-2">
-                    <MdHolidayVillage className="text-xl" />
-                    <Link href={`#`}>
-                        holiday
-                    </Link>
-                </li>
-                <li className="flex justify-start items-center gap-2">
-                    <ImProfile className="text-xl" />
-                    <Link href={`/users`}>
-                        employees
-                    </Link>
-                </li>
-                <li className="flex justify-start items-center gap-2">
-                    <RiUserSearchFill className="text-xl" />
-                    <Link href={`reqruitment`}>
-                        recruitment
-                    </Link>
-                </li>
-                <li className="flex justify-start items-center gap-2">
-                    <RiFileCopy2Fill className="text-xl" />
-                    <Link href={`applications`}>
-                    applications
-                    </Link>
-                </li>
-            </ul>
-            <div className="flex justify-start items-center  gap-4 border-t border-black pt-3 mt-6">
-            <li className="flex justify-start items-center gap-2">
-                    <IoIosHome className="text-xl" />
-                    <Link href={`/`}>
-                        home
-                    </Link>
-                </li>
-            </div>
-        </div>
-    )
-}
+  const { user } = useContext(coreContext);
+  const [show, setshow] = useState(false);
+  return (
+    <div
+      className={`pl-5 bg-emerald-200 md:min-h-screen p-3 shadow-2xl relative ${
+        show ? "h-auto" : "h-[65px]"
+      } overflow-hidden`}
+    >
+      <div className="flex justify-start items-center  gap-4 border-b border-black pb-3">
+        <Image
+          className="w-10 h-10 object-cover rounded-full"
+          src={user?.photoURL}
+          height={500}
+          width={500}
+          alt="img"
+        />
+        <span className="flex flex-col justify-start items-start gap-0">
+          <p className="text-lg font-bold">{user?.displayName}</p>
+          <a className="-mt-1 text-xs" href="mailto:useremail">
+            {user?.email}
+          </a>
+        </span>
+      </div>
+      <TiThMenuOutline
+        onClick={() => setshow(!show)}
+        className="absolute transition-all md:hidden block top-6 right-4 text-2xl font-bold"
+      />
+      <ul className="pt-3 uppercase font-semibold flex flex-col gap-2 ">
+        <li className="flex justify-start items-center gap-2 hover:bg-emerald-500 hover:text-white duration-500 px-3 rounded-md ">
+          <CgProfile className="text-xl" />
+          <Link className="p-3 w-full" href={`/dashboard`}>
+            profile
+          </Link>
+        </li>
+        <li className="flex justify-start items-center gap-2 hover:bg-emerald-500 hover:text-white duration-500 px-3 rounded-md ">
+          <MdHolidayVillage className="text-xl" />
+          <Link className="p-3 w-full" href={`/dashboard/holiday`}>
+            holiday
+          </Link>
+        </li>
+        <li className="flex justify-start items-center gap-2 hover:bg-emerald-500 hover:text-white duration-500 px-3 rounded-md ">
+          <ImProfile className="text-xl" />
+          <Link className="p-3 w-full" href={`/dashboard/employees`}>
+            employees
+          </Link>
+        </li>
+        <li className="flex justify-start items-center gap-2 hover:bg-emerald-500 hover:text-white duration-500 px-3 rounded-md ">
+          <RiUserSearchFill className="text-xl" />
+          <Link className="p-3 w-full" href={`/dashboard/recruitment`}>
+            recruitment
+          </Link>
+        </li>
+        <li className="flex justify-start items-center gap-2 hover:bg-emerald-500 hover:text-white duration-500 px-3 rounded-md ">
+          <RiFileCopy2Fill className="text-xl" />
+          <Link className="p-3 w-full" href={`/dashboard/applications`}>
+            applications
+          </Link>
+        </li>
+      </ul>
+      <div className="flex justify-start items-center  gap-4 border-t border-black pt-3 mt-6">
+        <li className="flex justify-start items-center w-full gap-2 hover:bg-emerald-500 hover:text-white duration-500 px-3 rounded-md ">
+          <IoIosHome className="text-xl" />
+          <Link className="p-3 w-full" href={`/`}>
+            home
+          </Link>
+        </li>
+      </div>
+    </div>
+  );
+};
 
-export default Menuber
+export default Menuber;
--- src/app/(dashboardlayout)/dashboard/page.js
+++ src/app/(dashboardlayout)/dashboard/page.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import ProfilePage from '@/Components/SharedComponents/ProfilePage'
 
 const dashboardmain = () => {
   return (
-    <div className='text-4xl text-center py-10 uppercase font-bold bg-gray-50 min-h-screen w-full pr-5 bg-opacity-40'>
-     profile page
+    <div className=''>
+     <ProfilePage></ProfilePage>
     </div>
   )
 }
