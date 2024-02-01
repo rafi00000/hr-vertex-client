72ae7e09eb8d5dfa05dc7d8e956fbd698732ddd3
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-02-01 11:20:31 +0600

    redesign notice and notice details page

--- src/app/(withlayout)/notice/page.js
+++ src/app/(withlayout)/notice/page.js
@@ -11,15 +11,19 @@ const usePage = async () => {
         // console.log(data);
         return (
             <>
-                <h3 className="text-center pt-10 uppercase text-3xl font-bold">All RECRUITMENT notice </h3>
-                <div className='container mx-auto sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 gap-y-6  py-10'>
+                <div className=" bg-[url('https://i.ibb.co/N1VHksh/Black-And-Red-Modern-Business-Human-Resource-Management-Presentation.png')] md:py-36 py-48">
+
+                    <p className="md:text-5xl text-3xl font-bold text-center block uppercase text-white">All RECRUITMENT notice</p>
+                </div>
+                <div className='container mx-auto py-10'>
+                {/* <div className='container mx-auto sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 gap-y-6  py-10'> */}
                     {
-                        data?.map(item => <div className='bg-[url("https://i.ibb.co/6v00Ygs/recruitment-process1.jpg")] bg-opacity-45 shadow-xl rounded p-4' key={item?._id}>
+                        data?.map(item => <div className='bg-opacity-45 shadow-sm rounded p-4 my-4 border' key={item?._id}>
                             <h3 className='text-xl font-bold'>{item?.position}</h3>
                             <p className='text-gray-500 font-semibold py-1'> department : {item?.department}</p>
-                            <p className='pb-2 opacity-80'>responsibilities :  {item?.responsibilities.slice(0,150)}</p>
+                            <p className='pb-2 opacity-80'>responsibilities :  {item?.responsibilities.slice(0, 150)}</p>
                             <Link className='flex justify-end items-center pt-2' href={`notice/${item?._id}`}>
-                               <Button text=' details' />
+                                <Button text=' details' />
                             </Link>
                         </div>)
                     }
