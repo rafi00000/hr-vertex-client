1370a7565a09ce9c7eab6aeb080c0d46c2c40417
Riad123456789 <riad80717@gmail.com>
2024-02-03 11:20:55 +0600

    add

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -2,33 +2,38 @@
 
 import { coreContext } from "@/provider/AuthContext";
 import { useContext, useState } from "react";
-
+import Button from '@/Components/ClintSideComponents/Button';
+import useAxiosSecure from "@/axiosConfig/useAxiosSecure";
 
 
 const modal = () => {
 
     const { user } = useContext(coreContext)
+    // console.log(user)
+    const axiosSecure = useAxiosSecure();
 
-    console.log(user)
-    const [formData, setFormData] = useState({
-        fullName: '',
-        emailAddress: '',
-        phoneNumber: '',
-        address: '',
-        education: '',
-        workExperience: '',
-        cv: '',
-    });
-
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(formData);
-    };
+        const form = e.target;
+        const fullName = form.fullName.value;
+        const emailAddress = form.emailAddress.value;
+        const phoneNumber = form.phoneNumber.value;
+        const address = form.address.value;
+        const education = form.education.value;
+        const workExperience = form.workExperience.value;
+        const cv = form.cv.value;
 
+        const data = {
+            fullName, address, phoneNumber, emailAddress, education, workExperience, cv
+        }
+        console.log(data);
+
+       
+    }
 
     return (
         <div>
-            <label htmlFor="my_modal_6" className="btn btn-accent">Apply Now</label>
+            <label htmlFor="my_modal_6" className="btn bg-emerald-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300">Apply Now</label>
             <input type="checkbox" id="my_modal_6" className="modal-toggle" />
             <div className="modal" role="dialog">
                 <div className="modal-box">
@@ -41,7 +46,6 @@ const modal = () => {
                                     defaultValue={user?.displayName}
                                     className="input input-bordered"
                                     name="fullName"
-                                    value={formData.fullName}
                                     required
                                 />
                             </div>
@@ -52,7 +56,6 @@ const modal = () => {
                                     defaultValue={user?.email}
                                     className="input input-bordered"
                                     name="emailAddress"
-                                    value={formData.emailAddress}
                                     required
                                 />
                             </div>
@@ -63,7 +66,6 @@ const modal = () => {
                                     placeholder="Enter your phone number"
                                     className="input input-bordered"
                                     name="phoneNumber"
-                                    value={formData.phoneNumber}
                                     required
                                 />
                             </div>
@@ -73,7 +75,6 @@ const modal = () => {
                                     placeholder="Enter your address"
                                     className="input input-bordered"
                                     name="address"
-                                    value={formData.address}
                                     required
                                 />
                             </div>
@@ -84,7 +85,6 @@ const modal = () => {
                                     placeholder="Enter your education background"
                                     className="input input-bordered"
                                     name="education"
-                                    value={formData.education}
                                     required
                                 />
                             </div>
@@ -94,7 +94,6 @@ const modal = () => {
                                     placeholder="Enter your work experience"
                                     className="input input-bordered"
                                     name="workExperience"
-                                    value={formData.workExperience}
                                     required
                                 />
                             </div>
@@ -105,17 +104,16 @@ const modal = () => {
                                     placeholder="Enter your link"
                                     className="input input-bordered"
                                     name="cv"
-                                    value={formData.cv}
                                     required
                                 />
                             </div>
                             <div className="form-control w-full">
-                                <button type="submit" className="btn btn-accent mt-10">Submit Application</button>
+                                <button type="submit" className="btn bg-emerald-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 mt-10">Submit Application</button>
                             </div>
                         </form>
                     </div>
                     <div className="modal-action">
-                        <label htmlFor="my_modal_6" className="btn">Close!</label>
+                        <label htmlFor="my_modal_6" className="w-full btn bg-emerald-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300">Close!</label>
                     </div>
                 </div>
             </div>
