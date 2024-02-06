9f0a2ea4151ce6ed171a70a4f8650c57a77f7e50
Rafi <delwar2021bd@gmail.com>
2024-02-06 07:13:25 +0600

    added attendance table

--- src/app/employeeLogin/page.js
+++ src/app/employeeLogin/page.js
@@ -1,8 +1,15 @@
 "use client"
 
+import { coreContext } from "@/provider/AuthContext";
+import { useRouter } from "next/navigation";
+import { useContext, useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import Swal from "sweetalert2";
+
 const EmployeeLogin = () => {
-
-
+  const { logIn } = useContext(coreContext);
+  const router = useRouter();
+  const [show, setShow] = useState(false)
     const handleEmployeeLogin = (e) =>{
         e.preventDefault();
         const form = e.target;
@@ -10,33 +17,32 @@ const EmployeeLogin = () => {
         const password = form.password.value;
         try {
         logIn(email, password)
-            .then((userCredential) => {
-            const user = userCredential.user;
+            .then((data) => {
+            const user = data.user;
             Swal.fire({
-            position: "top-end",
+            position: "center",
             icon: "success",
             title: `logged in user succesfuly`,
             showConfirmButton: false,
             timer: 1500,
           });
-          router.push("/");
+          router.push("/dashboard");
         })
         .catch((error) => {
           Swal.fire({
             icon: "error",
-            title: "Oops...",
+            title: "Wrong Credentials",
             text: `${error.message}`,
-            footer: "unable to log in user",
           });
         });
     } catch (error) {
       console.log(error);
     }
-    }
+  }
 
     return (
         <div className="bg-[url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover min-h-screen flex flex-col justify-center items-center">
-          <form className="space-y-4 my-10 w-4/5 md:w-1/3 lg:w-1/2 p-10 glass rounded-lg" onSubmit={handleEmployeeLogin}>
+          <form className="space-y-4 my-10 w-4/5 md:w-1/2 p-10 glass rounded-lg" onSubmit={handleEmployeeLogin}>
             <h1 className="text-4xl font-bold text-white text-center">Employee Login</h1>
             <div className="form-control w-full">
               <label className="text-md font-semibold">Email</label>
@@ -45,16 +51,25 @@ const EmployeeLogin = () => {
                 placeholder="Enter your email"
                 className="input input-bordered"
                 name="email"
+                required
               />
             </div>
-            <div className="form-control w-full">
+
+            <div className="form-control w-full relative">
               <label className="text-md font-semibold">Password</label>
               <input
-                type="password"
+                type={show ? "text" : "password"}
                 placeholder="Enter your password"
                 className="input input-bordered"
+                required
                 name="password"
               />
+              <span
+                className="absolute top-10 right-3"
+                onClick={() => setShow(!show)}
+              >
+                {show ? <FaEyeSlash className="cursor-pointer"></FaEyeSlash> : <FaEye className="cursor-pointer"></FaEye>}
+              </span>
             </div>
 
             <div className="flex  justify-center">
